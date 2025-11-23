e91634aaf5897322ecda358982fdca2375dc781d
AgustinaAyelenCordoba <165093267+AgustinaAyelenCordoba@users.noreply.github.com>
2025-11-23 20:10:12 -0300

    Moneda
    se agrego los efectos de sonido y se creo el prefab de moneda

--- ProyectoFinal/Assets/Obstaculos/Puas/CollicionPuas.cs
+++ ProyectoFinal/Assets/Obstaculos/Puas/CollicionPuas.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CollicionPuas : MonoBehaviour
 {
+    [SerializeField] private AudioClip AudioClip; 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+            AudioManager.Instance.PlaySoundEffect(AudioClip, 0.5f);
         }
     }
 
