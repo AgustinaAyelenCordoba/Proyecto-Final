ff33555f0c8f1e54b2ab69909e666e7706271bb5
AgustinaAyelenCordoba <165093267+AgustinaAyelenCordoba@users.noreply.github.com>
2025-11-27 20:39:39 -0300

    Creacion de Victoria ,puntaje y Cambios
    Se creo la ensena Victoria también ese agrego puntaje y se modifico el segundo nivel.

--- ProyectoFinal/Assets/Moneda/Script/MonedaCollider.cs
+++ ProyectoFinal/Assets/Moneda/Script/MonedaCollider.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MonedaCollider : MonoBehaviour
 {
    [SerializeField] private AudioClip AudioClip;
+    private int puntaje=1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -10,7 +11,7 @@ public class MonedaCollider : MonoBehaviour
         
             Destroy(gameObject);
             AudioManager.Instance.PlaySoundEffect(AudioClip,0.5f);
-        
+            GameManager.Instance.mostrarPuntaje(puntaje);
         }
     }
 
